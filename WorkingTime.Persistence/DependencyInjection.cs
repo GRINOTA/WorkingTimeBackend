0cd35b8e925fb6489fd297f3e526a7ddf0ccd500
GRINOTA <grinota.gregory@gmail.com>
2024-05-03 00:40:39 +0300

    added dependency injection

---
+++ WorkingTime.Persistence/DependencyInjection.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using WorkingTime.Application.Interfaces;
+
+namespace WorkingTime.Persistence
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddPersistence(this IServiceCollection services, IConfiguration configuration)
+        {
+            var connectionString = configuration["DbConnection"];
+            services.AddDbContext<WorkingTimeDbContext>(options =>
+            {
+                options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+            });
+            services.AddScoped<IWorkingTimeDbContext>(provider => provider.GetService<WorkingTimeDbContext>());
+            return services;
+        }
+    }
+}
