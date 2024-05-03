9ec8673e0ae95fe0abb0004a74f982e48dcfd4be
GRINOTA <grinota.gregory@gmail.com>
2024-05-04 02:26:14 +0300

    added base controller

---
+++ WorkingTime.WebAPI/Controllers/BaseController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using WorkingTime.Domain.Models;
+using System.Security.Claims;
+
+namespace WorkingTime.WebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]/[action]")]
+    public abstract class BaseController : ControllerBase
+    {
+        private IMediator _mediator;
+        protected IMediator Mediator => 
+            _mediator ??=HttpContext.RequestServices.GetService<IMediator>();
+
+        internal int EmployeeId => !User.Identity.IsAuthenticated ? 0 : int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value);
+
+    }
+}
